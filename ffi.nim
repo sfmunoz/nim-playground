f5280385b3d9f03682d3a1d624276ffd646ecfb1
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2023-09-15 18:05:51 +0000

    ffi.nim: c_malloc()/c_free() wrappers added

--- ffi.nim
+++ ffi.nim
@@ -8,15 +8,17 @@
 #   $ nim c -r ffi.nim
 #   $ valgrind ./ffi
 #
-# Ref (it's a free chapter; Google 'nim ffi'):
-#   https://livebook.manning.com/book/nim-in-action/chapter-8/51
+# Refs:
+#   https://livebook.manning.com/book/nim-in-action/chapter-8/51 (it's a free chapter; Google 'nim ffi')
+#   <nim-2.0.0>/lib/std/private/syslocks.nim -> c_malloc()/c_free()
 #
 
 # {{{ imports
 
 # The orthodox way:
-# import std/strformat, std/logging, std/osproc, std/streams
-import pure/logging
+# import std/logging
+import pure/logging, pure/strformat
+import std/envvars
 
 var cl = newConsoleLogger(fmtStr="$datetime [$levelid] ",useStderr=true)
 setLogFilter(lvlInfo)
@@ -79,15 +81,38 @@ proc time(arg: ptr CTime): CTime {.importc, header: "<time.h>".}
 proc localtime(time: ptr CTime): ptr TM {.importc, header: "<time.h>".}
 
 # }}}
+# {{{ c_malloc()
+
+# ref: lib/std/private/syslocks.nim -> c_malloc()
+# c-proto: void *malloc(size_t size);
+proc c_malloc(size: csize_t): pointer {.importc: "malloc", header: "<stdlib.h>".}
+
+# }}}
+# {{{ c_free()
+
+# ref: lib/std/private/syslocks.nim -> c_free()
+# c-proto: void free(void *ptr);
+proc c_free(p: pointer) {.importc: "free", header: "<stdlib.h>".}
+
+# }}}
 # {{{ main()
 
 proc main() =
-  discard printf("printf() ...... string = '%s' and int = '%03d'\n", "the_string", 1)
-  printf2("printf2() ..... string = '%s' and int = '%03d'\n", "the_string", 2)
+  discard printf("printf() .... string = '%s' and int = '%03d'\n", "the_string", 1)
+  printf2("printf2() ... string = '%s' and int = '%03d'\n", "the_string", 2)
   let secs = time(nil)
-  echo "time() ........ ",secs,"s"
+  info("time() ........ ",secs,"s")
   let tm = localtime(addr secs)  # no need to use 'free(tm)': it's static; 'free()' wrapper must be created/used if needed
-  echo "localtime() ... ",tm.the_hour,":",tm.tm_min
+  info("localtime() ... ",tm.the_hour,":",tm.tm_min)
+  let bsize: csize_t = 1024
+  let p = c_malloc(bsize)
+  let pa = addr p
+  info("c_malloc() .... ",p.repr," (",pa.repr,") -> ",bsize," bytes")
+  if getEnv("FFI_LEAK","0") == "1":
+    warn(&"c_free() ...... leaking '{bsize}' bytes (FFI_LEAK=1): use 'FFI_LEAK=1 valgrind ./ffi' to check the problem")
+  else:
+    info(&"c_free() ...... freeing '{bsize}' bytes (FFI_LEAK!=1): use 'valgrind ./ffi' to check everything is OK")
+    c_free(p)
 
 # }}}
 # ======== main ========
