fda826c2fd7a110b177b3cbc52e9faa13a8aecb4
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2023-09-22 18:00:06 +0000

    macros.nim: html example created (to-be-fixed)

--- macros.nim
+++ macros.nim
@@ -7,17 +7,19 @@
 # Compile/run:
 #   $ nim c -r macros.nim
 #
-# Ref:
-#   https://nim-lang.org/docs/manual.html#macros
+# Refs:
+#   - https://nim-lang.org/docs/manual.html#macros
+#   - https://nim-lang.org/docs/tut3.html
+#   - https://nim-by-example.github.io/macros/
 #
 
 # {{{ imports
 
-import std/macros
+import std/[macros, strutils]
 
 # }}}
-# ======== macros ========
-# {{{ debug()
+# ======== macros/procs ========
+# {{{ debug() -- macro
 
 macro debug(args: varargs[untyped]): untyped =
   result = nnkStmtList.newTree()
@@ -27,16 +29,101 @@ macro debug(args: varargs[untyped]): untyped =
     result.add newCall("writeLine", newIdentNode("stdout"), n)
 
 # }}}
-# ======== procs ========
-# {{{ main()
+# {{{ html_st() -- proc
+
+proc html_st(n: NimNode, depth: Natural): string =
+  #echo "===="
+  #echo n.treeRepr
+  #echo n.repr," | ",n.kind
+  #echo "----"
+  result = ""
+  if n.kind == nnkIdent:
+    result.add spaces(depth) & "<" & n.repr & ">\n"
+  for n2 in n.children:
+    result.add html_st(n2,depth+1)
+  if n.kind == nnkIdent:
+    result.add spaces(depth) & "</" & n.repr & ">\n"
+
+# }}}
+# {{{ html() -- macro
+
+macro html(head, body: untyped): untyped =
+  # dumptree:
+  #   html the_page:
+  #     head:
+  #       title "this is the title"
+  #     body:
+  #       ul:
+  #         li "one item"
+  #         li "another item"
+  # ----------------------------------------
+  # StmtList
+  #   Command
+  #     Ident "html"
+  #     Ident "the_page"
+  #     StmtList
+  #       Call
+  #         Ident "head"
+  #         StmtList
+  #           Command
+  #             Ident "title"
+  #             StrLit "this is the title"
+  #       Call
+  #         Ident "body"
+  #         StmtList
+  #           Call
+  #             Ident "ul"
+  #             StmtList
+  #               Command
+  #                 Ident "li"
+  #                 StrLit "one item"
+  #               Command
+  #                 Ident "li"
+  #                 StrLit "another item"
+
+  let buf = "<html>\n" & html_st(body,1) & "</html>"
+
+  # dumptree:
+  #   var buf: string = "...html..."
+  # ---------------------------------
+  # StmtList
+  #   VarSection
+  #     IdentDefs
+  #       Ident "buf"
+  #       Ident "string"
+  #       StrLit "...html..."
+
+  #result = nnkStmtList.newTree()
+  #result.add newNimNode(nnkVarSection).add(
+  #  newIdentDefs(ident(head.repr), ident("string"), newLit(buf))
+  #)
+
+  result = quote do:
+    let `head.repr` = `buf`
+
+  #echo result.treeRepr
+
+# }}}
+# {{{ main() -- proc
 
 proc main() =
+  echo "--------"
   var
     a: array[0..10, int]
     x = "some string"
   a[0] = 42
   a[1] = 45
   debug(a[0], a[1], x)
+  html the_page:
+    head:
+      title "this is the title"
+    body:
+      ul:
+        li "one item"
+        li "another item"
+  echo "--------"
+  echo the_page
+  echo "--------"
 
 # }}}
 # ======== main ========
