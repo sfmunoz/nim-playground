f6106d3f514d7b9fd9d75a382c9ff388ef5ab0d7
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2023-10-14 11:25:10 +0000

    httpd_mummy: simple server created

--- httpd_mummy/httpd_mummy.nimble
+++ httpd_mummy/httpd_mummy.nimble
@@ -3,6 +3,7 @@
 version       = "0.1.0"
 author        = "https://github.com/sfmunoz/nimex"
 description   = "httpd_mummy"
+license       = "GPL-2.0-or-later"
 srcDir        = "src"
 bin           = @["httpd_mummy"]
 
@@ -10,3 +11,4 @@ bin           = @["httpd_mummy"]
 # Dependencies
 
 requires "nim >= 2.0.0"
+requires "mummy >= 0.3.4"
--- httpd_mummy/src/httpd_mummy.nim
+++ httpd_mummy/src/httpd_mummy.nim
@@ -1,5 +1,54 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+#
+# vim: set foldmethod=marker:
+#
+# URL:  https://github.com/sfmunoz/nimex
+# Date: Sat Oct 14 11:11:06 AM UTC 2023
+#
+# Install deps (mummy):
+#   $ nimble refresh
+#   $ nimble install -d
+#
+# Compile/run:
+#   $ nimble run
+#
+# Client:
+#   $ curl http://127.0.0.1:8080
+#
+# Ref:
+#   https://github.com/guzba/mummy
+#
+
+# {{{ imports
+
+import mummy, mummy/routers
+
+# }}}
+# ======== procs ========
+# {{{ indexHandler() -- proc
+
+proc indexHandler(request: Request) =
+  var headers: HttpHeaders
+  headers["Content-Type"] = "text/plain"
+  request.respond(200, headers, "Hello, World!\n")
+
+# }}}
+# {{{ main() -- proc
+
+proc main() =
+  var router: Router
+  router.get("/", indexHandler)
+  let server = newServer(router)
+  let address = "127.0.0.1"
+  let port = 8080
+  let url = "http://" & address & ":" & $port
+  echo "Serving on ", url, "..."
+  server.serve(Port(port), address = address)
+
+# }}}
+# ======== main ========
+# {{{ main
 
 when isMainModule:
-  echo("Hello, World!")
+  main()
+
+# }}}
